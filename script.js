6de5802ca99d13abe64f68a9d2e94d1e303302dd
hengmhs <31433170+hengmhs@users.noreply.github.com>
2021-05-27 03:18:26 +0800

    Add flag display and news headlines API

--- script.js
+++ script.js
@@ -1,17 +1,38 @@
-const getCountryInfo = async (countryName) => {
-    const response = await fetch('https://restcountries.eu/rest/v2/alpha/' + countryName);
-    const countryInfo = await response.json();
+const searchCountryInfo = async (countryName) => {
     
-    console.log(countryInfo);
+    const img = document.getElementById('flag');
 
-    //population
-    console.log(countryInfo.population);
+    const response = await fetch('https://restcountries.eu/rest/v2/name/' + countryName);
+    const countryInfo = await response.json();
 
-    //capital
-    countryInfo.capital;
+    //countryInfo is returned as an array
+    console.log(countryInfo[0].capital);
 
     //flag image
-    countryInfo.flag;
+    img.src = countryInfo[0].flag;
+
+    //acronym form to be passed to News API
+    countryAcronym = countryInfo[0].altSpellings[0];
+
+    getHeadlines(countryAcronym);
 }
 
-getCountryInfo('USA');
+const getCountryInfo = () => {
+    const countryName = document.getElementById('country').value;
+    return searchCountryInfo(countryName);
+}
+
+const getHeadlines = async (countryAcronym) => {
+    const headlines = document.getElementById('headlines');
+    headlines.innerHTML = '';
+    const response = await fetch('https://gnews.io/api/v4/top-headlines?token=' + API_KEY + '&country=' + countryAcronym.toLowerCase())
+    const news = await response.json();
+    for (const article of news.articles){
+        console.log(article.title);
+        let headline = document.createElement('li');
+        headline.appendChild(document.createTextNode(article.title));
+        headlines.appendChild(headline);
+    }
+}
+
+getCountryInfo();
